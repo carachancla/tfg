0e5f84a265ea67843edccec1ac6d0d5fffb70627
carachancla <pauserra.org@gmail.com>
2021-05-28 21:18:27 +0200

    some progress

--- private/runs/W_0_oneToOneTrafficLimit/testChannelLimit.sh
+++ private/runs/W_0_oneToOneTrafficLimit/testChannelLimit.sh
@@ -20,8 +20,8 @@ run_folder=W_0_oneToOneTrafficLimit/jellyfish_n2_d1_wireless_flows_
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 
 flows_per_s=100
-#cd ../../.. ;java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}
-#exit
+cd ../../.. ;java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}
+exit
 ssh ${machine} "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
 flows_per_s=200
