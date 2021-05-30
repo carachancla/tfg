aed45bb3adfa0188701bc6a0eeadd37e9654c25e
carachancla <pauserra.org@gmail.com>
2021-05-30 12:18:26 +0200

    some progress

--- private/runs/W_1_trafficLimit_wireless/testChannelLimit.sh
+++ private/runs/W_1_trafficLimit_wireless/testChannelLimit.sh
@@ -20,13 +20,9 @@ run_folder=W_1_trafficLimit_wireless/jellyfish_n128_d30_wireless_flows_
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 
 flows_per_s=100
-cd ../../.. ;java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}
-exit
+#cd ../../.. ;java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}
+#exit
 ssh ${machine} "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
-
-flows_per_s=50
-#runtime=`calc ${num_flows} / ${flows_per_s}`;
-ssh ${machine} "cd ${netbechPath}; screen -L -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
 flows_per_s=200
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
@@ -36,19 +32,16 @@ flows_per_s=400
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 ssh ${machine} "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
-flows_per_s=800
+flows_per_s=600
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
+
 
 flows_per_s=1000
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
-flows_per_s=5000
-#runtime=`calc ${num_flows} / ${flows_per_s}`;
-ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
-
-flows_per_s=10000
+flows_per_s=2000
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
