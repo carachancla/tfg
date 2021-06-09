7c2f52b99eb93464f1474d52f2b5f0a9e8f2dccb
carachancla <pauserra.org@gmail.com>
2021-06-10 00:14:54 +0200

    some progress

--- private/runs/W_1_trafficLimit_hybrid/ethernetJellifishLimit.sh
+++ private/runs/W_1_trafficLimit_hybrid/ethernetJellifishLimit.sh
@@ -46,5 +46,11 @@ flows_per_s=10000
 #runtime=`calc ${num_flows} / ${flows_per_s}`;
 ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
+flows_per_s=50000
+#runtime=`calc ${num_flows} / ${flows_per_s}`;
+ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
+flows_per_s=80000
+#runtime=`calc ${num_flows} / ${flows_per_s}`;
+ssh ${machine}  "cd ${netbechPath}; screen -d -m java -ea -jar NetBench.jar ${properties_file} run_folder_name=${run_folder}${flows_per_s}_runtime_${runtime}s traffic_lambda_flow_starts_per_s=${flows_per_s} run_time_s=${runtime}"
 
 echo "runs initialized"
